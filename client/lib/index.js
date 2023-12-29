c4e0e7adf50aed47e583b28b9e9b3937d48e069e
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-12-29 19:51:43 +0100

    Create TCP connection

--- client/lib/index.js
+++ client/lib/index.js
@@ -1,8 +1,41 @@
+const Net = require('net');
+const { log } = console;
+
 class Client {
     constructor(ip, port, password) {
         this.ip = ip;
         this.port = port;
         this.password = password;
+
+    }
+
+    async connect() {
+        var a;
+        a = this;
+        const client = new Net.Socket();
+        client.connect({ port: port, host: host }), function() {
+            log(`> Connected to ${ip}:${port}`);
+
+            if (a.onConnect) a.onConnect();
+        });
+        
+        client.on('data', function(chunk) {
+            if (a.onData) a.onData(chunk);
+            // client.end();
+        });
+        
+        client.on('end', function() {
+            if (a.onEnd) a.onEnd();
+            console.log('Requested an end to the TCP connection');
+        });
+
+        this.conn = client;
+    }
+
+    async onConnect() {
+        const client = this.client;
+
+        
     }
 }
 
