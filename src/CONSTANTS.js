7530e97a12de759c86320ec18d5855b4f9ef4a2e
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-12-29 16:37:59 +0000

    Error handling

---
+++ src/CONSTANTS.js
@@ -0,0 +1,6 @@
+var config = {};
+
+config.BaseDir = '/etc/xedadb';
+config.ConfigPath = config.BaseDir + '/config.yml';
+
+module.exports = config;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 const net = require('net');
 
+const CONSTANTS = require('./CONSTANTS');
+
+function handleError(err, error, exit) {
+  console.error(`> Error: ${err} - ${String(error)}`);
+
+  if (exit) {
+    console.log(`> Major error. Exiting...`);
+    process.exit(1);
+  } else {
+    console.log(`> Error detected. Ignoring...`);
+  }
+}
+
 function loadConfig(filePath) {
   try {
     const fileContents = fs.readFileSync(filePath, 'utf8');
     const config = yaml.load(fileContents);
     return config;
   } catch (error) {
-    console.error(`Err loading cfg file: ${filePath}`);
-    console.error(error.message);
-    process.exit(1);
+    handleError(`Failed to load config file at ${filePath}`, error, true);
   }
 }
 
