5defa3691e881e3515c254a64d2b130293ecfc93
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-12-29 16:43:21 +0000

    Start with client

---
+++ client/index.js
@@ -0,0 +1,3 @@
+const Client = require('./lib');
+
+const db = new Client('127.0.0.1', 1895, 'mydb');
---
+++ client/lib/index.js
@@ -0,0 +1,9 @@
+class Client {
+    constructor(ip, port, password) {
+        this.ip = ip;
+        this.port = port;
+        this.password = password;
+    }
+}
+
+module.exports = Client;
