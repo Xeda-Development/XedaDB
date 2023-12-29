c035c1e09ec78fad3ec9fcb6674e6a4829f12503
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-12-29 19:32:02 +0000

    Send some packets

--- src/client.js
+++ src/client.js
@@ -4,22 +4,34 @@ class DBClient {
     constructor(server, socket) {
         this.server = server;
         this.socket = socket;
+
         this.ID = Math.floor(Math.random() * 1000);
+        this.changeState('PRE');
 
         this.sendPacket('AUTH');
+        this.sendPacket('CLIENTDATA', [
+            this.ID,
+            this.state
+        ]);
     }
 
-    async onData(data) {
+    changeState(state) {
+        this.state = state;
+        this.sendPacket('STATE', [state]);
+    }
+
+    onData(data) {
         console.log(`> [${this.ID}] Got data from ${this.socket.address().address}:${this.socket.address().port}: ${json.stringify(data)}`);
     }
 
-    async sendPacket(packetId, args) {
+    sendPacket(packetId, args) {
         if (!packetId) packetId = 'UNKNOWN';
         if (!args) args = [];
         this.socket.write(pack.encode([
             packetId,
             args
         ]));
+        return true;
     }
 }
 
