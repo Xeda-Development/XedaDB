0bc76b52a1d8a4907be061af6b9ce925b2ee408a
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-12-30 12:34:39 +0000

    Rewrite

--- testing/index.js
+++ testing/index.js
@@ -1,62 +1,150 @@
-// Sample NoSQL Database
-const MyNoSQLDB = {
-    // Collection for storing user data
-    users: [
-      { id: 1, name: 'Alice', age: 25 },
-      { id: 2, name: 'Bob', age: 30 },
-      { id: 3, name: 'Charlie', age: 22 },
-      // ... more user documents
-    ],
-  
-    // Index for quick lookup by user name
-    indexByName: {},
-  };
-  
-  // Function to create an index on a specific field
-  function createIndex(collection, fieldName) {
-    const index = {};
+const fs = require("fs");
 
-    var i;
-    i = 0;
-  
-    // Populate the index
-    collection.forEach((document) => {
-      const key = document[fieldName];
-      console.log(`Index ${i}`, document.id, key)
-    //   if (!index[key]) {
-    //     index[key] = [];
-    //   }
-    //   index[key].push(document);
-    if (!index[key]) index[key] = [];
+// Initial partition size limit
+const partitionSizeLimit = 2; // For example purposes, set a small partition limit
 
-    index[key].push(i);
-      i++;
-    });
-  
-    return index;
-  }
-  
-  // Create an index on the 'name' field for the 'users' collection
-  MyNoSQLDB.indexByName = createIndex(MyNoSQLDB.users, 'name');
-  
-  // Function to perform a quick lookup using an index
-  function findByField(collection, index, fieldName, value) {
-    const entries = index[value];
-    if (entries) {
-      // Return the matching entries
-      var res;
-      res = [];
-      for (let a=0; a<entries.length; a++) {
-        res.push(collection[a])
-      }
-      return res;
-    } else {
-      // No matching entries found
-      return [];
+// Sample database array
+let database = [
+  { id: 1, name: "John", age: 25 },
+  { id: 2, name: "Alice", age: 30 },
+  { id: 3, name: "Bob", age: 28 },
+  { id: 4, name: "Eve", age: 26 },
+  { id: 5, name: "Mark", age: 32 },
+  // Additional entries might be added dynamically
+];
+
+// Index object to store references for quick access
+let index = {};
+
+// Cache object to store frequently accessed data
+let cache = {};
+
+// Function to create an index based on a specific property
+function createIndex(property) {
+  index[property] = {};
+  for (let i = 0; i < database.length; i++) {
+    let entry = database[i];
+    if (!index[property][entry[property]]) {
+      index[property][entry[property]] = [];
     }
+    index[property][entry[property]].push(entry);
   }
-  
-  // Example: Find users with the name 'Bob' using the index
-  const results = findByField(MyNoSQLDB.users, MyNoSQLDB.indexByName, 'name', 'Bob');
-  console.log('data:', JSON.stringify(MyNoSQLDB, null, 2))
-  console.log('results:', results);
+}
+
+// Loading data from partitions instead of database.json
+loadFromPartitions();
+
+// Creating index on 'name', 'id', and 'age' properties
+createIndex("name");
+createIndex("id");
+createIndex("age");
+
+// Save index and cache to a file
+function saveToFile() {
+  const dataToSave = {
+    index,
+    cache,
+  };
+
+  fs.writeFileSync("database.json", JSON.stringify(dataToSave, null, 2));
+  console.log("Index and cache saved to file.");
+}
+
+// Load data from partitions to the database
+function loadFromPartitions() {
+  const loadedPartitions = loadPartitions();
+  database = loadedPartitions.flat(); // Flatten partitions into a single database
+}
+
+// Function to query by indexed property with caching
+function queryByProperty(property, value) {
+  const cacheKey = `${property}-${value}`;
+  if (cache[cacheKey]) {
+    console.log("Data retrieved from cache.");
+    return cache[cacheKey];
+  }
+
+  if (index[property] && index[property][value]) {
+    const result = index[property][value];
+    cache[cacheKey] = result;
+    console.log("Data retrieved from database and cached.");
+    return result;
+  }
+
+  return [];
+}
+
+// Create operation - Add a new entry to the database
+function createEntry(entry) {
+  database.push(entry);
+
+  // Check if the partition limit is exceeded, then split the database
+  if (database.length > partitionSizeLimit) {
+    partitionDatabase();
+  }
+
+  createIndex("name");
+  createIndex("id");
+  createIndex("age"); // Re-create index after insertion
+  saveToFile(); // Save index and cache to file after addition
+}
+
+// Function to partition the database and save partitions to disk
+function partitionDatabase() {
+  const totalEntries = database.length;
+  const numPartitions = Math.ceil(totalEntries / partitionSizeLimit);
+
+  const partitions = [];
+  for (let i = 0; i < numPartitions; i++) {
+    const start = i * partitionSizeLimit;
+    const end = start + partitionSizeLimit;
+    partitions.push(database.slice(start, end));
+  }
+
+  console.log("Database partitioned into:", partitions.length, "partitions");
+
+  // Save each partition to a separate file
+  partitions.forEach((partition, index) => {
+    const partitionFilename = `partition_${index + 1}.json`;
+    fs.writeFileSync(partitionFilename, JSON.stringify(partition));
+    console.log(`Partition ${index + 1} saved to ${partitionFilename}`);
+  });
+}
+
+// Function to load partitions from disk
+function loadPartitions() {
+  const loadedPartitions = [];
+  let partitionIndex = 1;
+  let partitionFilename = `partition_${partitionIndex}.json`;
+
+  while (fs.existsSync(partitionFilename)) {
+    const data = fs.readFileSync(partitionFilename, "utf8");
+    const parsedData = JSON.parse(data);
+    loadedPartitions.push(parsedData);
+    console.log(`Partition ${partitionIndex} loaded from ${partitionFilename}`);
+
+    partitionIndex++;
+    partitionFilename = `partition_${partitionIndex}.json`;
+  }
+
+  return loadedPartitions;
+}
+
+// Trigger partitioning after adding an entry to the database
+
+
+// Save the index and cache to disk
+
+
+// Load partitions from disk
+const loadedPartitions = loadPartitions();
+console.log("Loaded partitions:", loadedPartitions);
+
+createEntry({ id: 7, name: "Mau", age: 29 }); // Create
+
+
+console.log('query:', queryByProperty('name', 'Mau'));
+
+
+partitionDatabase()
+saveToFile();
