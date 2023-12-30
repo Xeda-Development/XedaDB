0b917334262f00f9337612653e039ff6e033611e
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-12-30 11:25:22 +0000

    Test

--- testing/storage.js
+++ testing/storage.js
@@ -45,6 +45,17 @@ let MyNoSQLDB = {
     { id: 2, name: 'Bob', age: 30 },
     { id: 3, name: 'Charlie', age: 22 },
     { id: 4, name: 'Bob', age: 22 },
+    { id: 5, name: 'Mau', age: 22 },
+    { id: 6, name: 'KAt', age: 22 },
+    { id: 7, name: 'test', age: 22 },
+    { id: 8, name: 'data', age: 22 },
+    { id: 9, name: 'pizza', age: 22 },
+    { id: 10, name: 'this is a test', age: 22 },
+    { id: 11, name: 'idk', age: 22 },
+    { id: 12, name: 'hee!', age: 22 },
+    { id: 13, name: 'hi everyone', age: 22 },
+    { id: 14, name: 'job', age: 22 },
+    { id: 15, name: 'karl', age: 22 },
     // ... more user documents
   ],
 
@@ -142,6 +153,6 @@ const resultsPartitioned = findByFieldPartitioned(
   MyNoSQLDB.users,
   MyNoSQLDB.hashIndexPartitions,
   'name',
-  'Bob'
+  'karl'
 );
 console.log('Results with Partitioned Hash-based Index using MD5:', resultsPartitioned);
