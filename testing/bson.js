15c2763a52ebed8bccef00b92efaa30690e75a56
Bas <55401336+BasToTheMax@users.noreply.github.com>
2023-12-30 12:34:32 +0000

    Remove old test code

--- testing/bson.js
+++
@@ -1,8 +0,0 @@
-const { BSON, EJSON, ObjectId } = require('bson');
-
-// const bytes = BSON.serialize({ _id: new ObjectId() });
-const bytes = BSON.serialize({ name: 'Max' });
-console.log(bytes, String(bytes));
-const doc = BSON.deserialize(bytes);
-console.log(EJSON.stringify(doc));
-// {"_id":{"$oid":"..."}}
